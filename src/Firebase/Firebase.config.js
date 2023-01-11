03a9ae5bd1770a59f6fb364eee14e7bc4d06fd5a
emon-webdev <emon.webdev@gmail.com>
2023-01-12 01:40:17 +0600

    header top complate

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDrG9kqOMINdvXAoxGjt8JDpXGHcIufxVQ",
+  authDomain: "capital-trust-bank-ee791.firebaseapp.com",
+  projectId: "capital-trust-bank-ee791",
+  storageBucket: "capital-trust-bank-ee791.appspot.com",
+  messagingSenderId: "709974636356",
+  appId: "1:709974636356:web:01388e1ece431d08d98911"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
