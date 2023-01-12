01e83d18da7f60029a439fda8ccaa258324746c4
wahid30 <md.wahidulalamjp2@gmail.com>
2023-01-12 12:23:53 +0600

    footer compo added

--- src/Pages/Shared/Footer.js
+++ src/Pages/Shared/Footer.js
@@ -4,25 +4,27 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="bgi-footer">
-      <h2>Footer</h2>
-      <h2>Footer</h2>
-      <h2>Footer</h2>
-      <h2>Footer</h2>
-      <h2>Footer</h2>
-      <h2>Footer</h2>
-      <h2>Footer</h2>
-      <h2>Footer</h2>
-      <h2>Footer</h2>
-      <h2>Footer</h2>
-      <h2>Footer</h2>
-      <h2>Footer</h2>
-      <h2>Footer</h2>
-      <h2>Footer</h2>
-      <h2>Footer</h2>
-      <h2>Footer</h2>
-      <h2>Footer</h2>
-      <h2>Footer</h2>
-      <h2>Footer</h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-5">
+        <div>logo</div>
+        <div>
+          <h2>Company</h2>
+        </div>
+        <div>
+          <h2>Contact</h2>
+        </div>
+        <div>
+          <h2>More </h2>
+        </div>
+        <div>
+          <div>
+            <h2>Head Office</h2>
+          </div>
+          <div>
+            <h2>News Letter</h2>
+          </div>
+        </div>
+      </div>
+      <div>hi</div>
     </div>
   );
 };
