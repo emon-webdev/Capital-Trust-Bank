8c0ecbcafe60eac9fb8a142b867d61a6dd8a7f9b
AkashChakrabortty <akashchakrabortty@gmail.com>
2023-01-12 14:02:47 +0600

    services with responsive

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Banner from "./Banner";
 import Counter from "./Counter";
+import Services from "./Services.jsx";
 
 const Home = () => {
   return (
     <div>
        <Banner />
+       <Services></Services>
        <Counter/>
+
     </div>
   );
 };
