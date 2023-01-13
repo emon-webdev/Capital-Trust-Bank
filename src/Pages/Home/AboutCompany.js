02597382698351bcc8cbc3b39540a50a5f01d9ab
emon-webdev <emon.webdev@gmail.com>
2023-01-13 16:42:05 +0600

    file create

---
+++ src/Pages/Home/AboutCompany.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutCompany = () => {
+    return (
+        <div>
+             <h2>About Company</h2>
+        </div>
+    );
+};
+
+export default AboutCompany;
