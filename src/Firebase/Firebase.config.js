3e26051258ae39614a6a97e3be8a40c2c6b7481f
MD Rakib Khan <contact.antor.bd@gmail.com>
2023-01-16 10:05:32 +0600

    commit

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6siCnoqkoKoxmq8bgsj0lOZaCuzVvzmc",
+  authDomain: "capital-trust-bank-5b91f.firebaseapp.com",
+  projectId: "capital-trust-bank-5b91f",
+  storageBucket: "capital-trust-bank-5b91f.appspot.com",
+  messagingSenderId: "1056020613970",
+  appId: "1:1056020613970:web:fc8ada302831d0050c488b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
