bc6a8e7e8c75e65186d69d5e0e33433543413d9e
MD Rakib Khan <contact.antor.bd@gmail.com>
2023-01-17 16:16:19 +0600

    annimation error create

--- src/Pages/Shared/Error.js
+++ src/Pages/Shared/Error.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import errorLottie from '../../assests/Error/error.json';
+import Lottie from "lottie-react";
 
 const Error = () => {
     return (
@@ -7,11 +9,10 @@ const Error = () => {
             <section className="flex items-center h-[100vh] p-16">
                 <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
                     <div className="max-w-md text-center">
-                        <h2 className="mb-8 font-extrabold text-9xl text-[#df0303]">
-                            <span className="sr-only">Error</span>404
+                        <h2 className="mb-4 font-extrabold text-9xl text-[#df0303]">
+                        <Lottie animationData={errorLottie} />                            
                         </h2>
-                        <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
-                        <p className="mt-4 mb-8 dark:text-gray-400">But dont worry, you can find plenty of other things on our homepage.</p>
+                        <p className="text-2xl font-semibold text-[#010c3a] md:text-3xl">Sorry, we couldn't find this page.</p>                        
                         <Link rel="noopener noreferrer" to="/" className="px-8 py-3 font-semibold rounded secondary-btn text-white">Back to homepage</Link>
                     </div>
                 </div>
