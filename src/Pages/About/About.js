1783cce5cb447ae86e73e81988ef749ed24a3343
wahid30 <md.wahidulalamjp2@gmail.com>
2023-01-19 12:28:28 +0600

    position added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,13 +1,24 @@
 import React from "react";
 import "./About.css";
+import shape1 from "../../assets/aboutUs/Vector.png";
+import shape2 from "../../assets/aboutUs/Vector2.png";
 import shape3 from "../../assets/aboutUs/bg-shape3.png";
+import shape4 from "../../assets/aboutUs/about_bg_two.png";
 
 const About = () => {
   return (
     <div className="about-parent h-60">
-      <div className="container about-sub-parent">
-        <h2 className="text-white p-28 text-2xl">About Us</h2>
-        <img className="about-sub-parent-img1 w-[130px]" src={shape3} alt="" />
+      <div className="container about-sub-parent  p-28">
+        <h2 className="text-white text-2xl">About Us</h2>
+        <div className="text-white flex gap-1 text-[12px]">
+          <span>Home</span>
+          <span>{">"}</span>
+          <span>About Us</span>
+        </div>
+        <img className="about-sub-parent-img1 w-[700px]" src={shape1} alt="" />
+        <img className="about-sub-parent-img2 w-[550px]" src={shape2} alt="" />
+        <img className="about-sub-parent-img3 w-[130px]" src={shape3} alt="" />
+        <img className="about-sub-parent-img4 w-[100px]" src={shape4} alt="" />
       </div>
     </div>
   );
