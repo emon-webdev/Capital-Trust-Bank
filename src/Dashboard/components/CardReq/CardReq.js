dfa5d91928212d19fb9de01a3bb2caed044768de
AkashChakrabortty <akashchakrabortty@gmail.com>
2023-01-24 22:00:09 +0600

    show card req customers

--- src/Dashboard/components/CardReq/CardReq.js
+++ src/Dashboard/components/CardReq/CardReq.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const CardReq = () => {
-    return <div>CardReq</div>;
+      const [customers, setCustomers] = useState([]);
+      useEffect(() => {
+        fetch(`http://localhost:5000/cardReq`)
+          .then((res) => res.json())
+          .then((data) => {
+            setCustomers(data);
+          });
+      }, []);
+    return (
+      <div>
+        {customers.map((customer) => {
+          return (
+            <div key={customer._id}>
+              <div className="w-1/2 mx-auto flex my-2 gap-2 p-2 border rounded">
+                <div className="flex items-center">
+                  <img
+                    className="h-16 w-16 rounded"
+                    src={customer.image}
+                    alt={customer.name}
+                  />
+                </div>
+                <div className="info flex flex-col overflow-auto">
+                  <span>Name: {customer.name}</span>
+                  <span>Email:{customer.email}</span>
+                  <span>Phone:{customer.phone}</span>
+                  <span>Account id:{customer.AccountId}</span>
+                  <div className="flex gap-2 p-2">
+                    <button className="ring rounded p-1">Give card</button>
+                    <button className="ring rounded p-1">Cancel</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
 };
 
 export default CardReq;
