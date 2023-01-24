0163639012f40d93ada8ada864af85f958100f99
AkashChakrabortty <akashchakrabortty@gmail.com>
2023-01-24 17:52:59 +0600

    Formspree

--- src/Dashboard/components/AllCustomers/AllCustomers.js
+++ src/Dashboard/components/AllCustomers/AllCustomers.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const AllCustomers = () => {
-    return <div>AllCustomers</div>;
+    const [customers,setCustomers] = useState([]);
+     useEffect(() => {
+       fetch(`http://localhost:5000/allCustomers`)
+         .then((res) => res.json())
+         .then((data) => {
+           setCustomers(data);
+           console.log(data);
+         });
+     }, []);
+    return <div>
+
+    </div>;
 };
 
 export default AllCustomers;
