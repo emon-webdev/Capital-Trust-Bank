67d84a50c8c43734cf91e74fbe2192fd5abc36da
Salma Tabassum Mouri <mouriclub@gmail.com>
2023-01-26 20:09:11 +0600

    dynamic banner added to insurance

--- src/Pages/Insurance/HealthInsurance.js
+++ src/Pages/Insurance/HealthInsurance.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import DynamicBanner from '../Shared/DynamicBanner/DynamicBanner';
 import HealthCard from './HealthCard'
 import HealthForm from './HealthForn'
 
 export default function HealthInsurance() {
+  const [name, setName] = useState("Health Insurance");
   return (
     <div>
+      <div className="">
+        <DynamicBanner  name={name}></DynamicBanner>
+      </div>
       <HealthCard></HealthCard>
       <HealthForm></HealthForm>
     </div>
--- src/Pages/Insurance/TravelInsurance.js
+++ src/Pages/Insurance/TravelInsurance.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import DynamicBanner from '../Shared/DynamicBanner/DynamicBanner';
 import TravelCard from './TravelCard'
 import TravelForm from './TravelForm'
 
 export default function TravelInsurance() {
+  const [name, setName] = useState("Travel Insurance");
   return (
     <div>
+         <div className="">
+      <DynamicBanner name={name}></DynamicBanner>
+      </div>
       <TravelCard></TravelCard>
       <TravelForm></TravelForm>
     </div>
