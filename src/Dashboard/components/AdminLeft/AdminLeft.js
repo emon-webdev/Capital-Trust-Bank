8238fb61470b2beab498e8d30a9c8bc829a50d0e
AkashChakrabortty <akashchakrabortty@gmail.com>
2023-01-27 03:58:14 +0600

    left nav hr

--- src/Dashboard/components/AdminLeft/AdminLeft.js
+++ src/Dashboard/components/AdminLeft/AdminLeft.js
@@ -2,6 +2,8 @@ import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import logo from '../../../assets/logo/mainlogo.png';
+
 const AdminLeft = () => {
    const [isDown, setIsDown] = useState(false);
    const [isX, setIsX] = useState(false);
@@ -28,7 +30,19 @@ const AdminLeft = () => {
           )}
         </button>
       </div>
-
+      <div className="hr flex w-11/12 mx-auto">
+        <div className="left-hr w-2/5">
+          <span>
+            <hr/>
+          </span>
+        </div>
+        <div className="logo">
+          <img src={logo} alt="logo" className="-mt-4"/> 
+        </div>
+        <div className="right-hr w-2/5">
+          <hr />
+        </div>
+      </div>
       <div className={`sideNav ${isX ? "block" : "hidden"} `}>
         <div className="menu p-2">
           <div className="flex flex-col divide-y-4 hover:divide-y-0 divide-red-600 lg:p-2">
