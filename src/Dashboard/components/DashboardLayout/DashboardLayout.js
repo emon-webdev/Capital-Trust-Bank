fb7a725c3faf56857a0de41a5048e79bf1c48567
AkashChakrabortty <akashchakrabortty@gmail.com>
2023-01-27 02:20:59 +0600

    bg added

--- src/Dashboard/components/DashboardLayout/DashboardLayout.js
+++ src/Dashboard/components/DashboardLayout/DashboardLayout.js
@@ -9,11 +9,11 @@ const DashboardLayout = () => {
   return (
     <div>
       <Header />
-      <div className="flex">
-        <div className="left w-1/4">
+      <div className="flex default-font h-fit">
+        <div className="left w-1/4 default-bg">
           <LeftContainer></LeftContainer>
         </div>
-        <div className="right w-full">
+        <div className="right w-full default-bg">
           <Outlet></Outlet>
         </div>
       </div>
