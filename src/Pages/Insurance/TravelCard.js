2e1a46ef542e79e5e58cf28e22042cef7d1c3ff7
Salma Tabassum Mouri <mouriclub@gmail.com>
2023-02-06 21:29:41 +0600

    insurance corrected

--- src/Pages/Insurance/TravelCard.js
+++ src/Pages/Insurance/TravelCard.js
@@ -1,3 +1,6 @@
+import { Card, CardBody } from "@chakra-ui/card";
+import { Text } from "@chakra-ui/layout";
+import {BsFillArrowRightCircleFill } from 'react-icons/bs'
 import React from "react";
 import { BiSupport } from "react-icons/bi";
 import { GiFocusedLightning, GiGloves } from "react-icons/gi";
@@ -28,18 +31,18 @@ export default function TravelCard() {
   ];
   return (
     <div className=" container flex align-center justify-center gap-6 my-20 flex-col md:flex-row">
-      {/* {traveldata.map((tra) => (
+      {traveldata.map((tra) => (
         <Card
           key={tra.id}
           style={{ width: "100%", height: "auto" }}
           className="hover:bg-[#041C51]  hover:text-[#fff]
     hover:animate-animated hover:animate-pulse"
         >
-          <CardContent className="flex flex-col align-center justify-center hover:text-white">
+          <CardBody className="flex flex-col align-center justify-center hover:text-white">
             <div className="w-100  text-4xl flex align-center justify-center my-3">
               <tra.icon />
             </div>
-            <Typography
+            <Text
               fontWeight={700}
               color="#DF0303"
               marginY={2}
@@ -47,14 +50,14 @@ export default function TravelCard() {
               textAlign="center"
             >
               {TravelCard.title}
-            </Typography>
+            </Text>
             <div className="text-md text-center">{tra.details}</div>
             <div className="w-100 text-sky-500 text-4xl flex align-center justify-center my-3">
               <BsFillArrowRightCircleFill />
             </div>
-          </CardContent>
+          </CardBody>
         </Card>
-      ))} */}
+      ))}
     </div>
   );
 }
