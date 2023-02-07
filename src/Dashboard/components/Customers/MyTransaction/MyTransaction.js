8a756c2c260b03119cc37dc69d9562b9d3b72b5b
Salma Tabassum Mouri <mouriclub@gmail.com>
2023-02-08 01:06:59 +0600

    add

--- src/Dashboard/components/Customers/MyTransaction/MyTransaction.js
+++ src/Dashboard/components/Customers/MyTransaction/MyTransaction.js
@@ -1,6 +1,6 @@
-
-
+import { Button, Table, TableCaption, TableContainer, Tbody, Td, Tfoot, Th, Thead, Tr } from "@chakra-ui/react";
 import React, { useState } from "react";
+import VisaTransaction from "./VisaTransaction";
 
 
 export default function MyTransaction() {
@@ -8,9 +8,53 @@ export default function MyTransaction() {
 
 
   return (
-    <div className="container mt-10 flex gap-5">
-      
+    <div className="container flex gap-10" >
+      <div className="">
+      <TableContainer borderRadius={10} backgroundColor='white' marginX={10} marginY={10}>
+  <Table variant='simple'>
+  
+    <Thead  >
+      <Tr >
+        <Th  color='#041C51'  fontSize={24} paddingY={6}>Category</Th>
+        <Th color='#041C51' fontSize={24} paddingY={6}>Date</Th>
+        <Th color='#041C51'  fontSize={24} paddingY={6}>Time</Th>
+        <Th color='#041C51' fontSize={24} paddingY={6} >Amount</Th>
+        <Th color='#041C51' fontSize={24} paddingY={6} >Details</Th>
+
+      </Tr>
+    </Thead>
+    <Tbody>
+      <Tr>
+        <Td>inches</Td>
+        <Td>millimetres (mm)</Td>
+        <Td>25.4</Td>
+        <Td>25.4</Td>
+        <Td><Button>Details</Button></Td>
+      </Tr>
+      <Tr>
+        <Td>feet</Td>
+        <Td>centimetres (cm)</Td>
+        <Td>25.4</Td>
+        <Td>25.4</Td>
+        <Td><Button>Details</Button></Td>
+      </Tr>
+      <Tr>
+        <Td>yards</Td>
+        <Td>metres (m)</Td>
+        <Td>25.4</Td>
+        <Td>25.4</Td>
+        <Td><Button>Details</Button></Td>
+      </Tr>
+    </Tbody>
+  
+  </Table>
+</TableContainer>
+      </div>
+      <div className="mt-2">
+        <VisaTransaction></VisaTransaction>
+      </div>
       
     </div>
   );
 }
+
