3aff23caa89cd52e04bbd72d04093c0215ccd786
emon-webdev <emon.webdev@gmail.com>
2023-02-08 09:35:00 +0600

    apply for credit card form complate

--- src/Pages/Home/ApplyCreditCard.js
+++ src/Pages/Home/ApplyCreditCard.js
@@ -1,75 +1,15 @@
 import { useDisclosure } from "@chakra-ui/hooks";
 import { ModalOverlay } from "@chakra-ui/modal";
-import React, { useContext } from "react";
-import { useForm } from "react-hook-form";
+import React, { useState } from "react";
 import { AiOutlineCheckCircle } from "react-icons/ai";
 import creditCardImg from "../../assets/apply-loan/credit-card-img.png";
 import CreditCardModal from "../../component/CreditCardModal.js/CreditCardModal";
-import { AuthContext } from "../../context/AuthProvider";
 
 const ApplyCreditCard = () => {
-  const { user, loading } = useContext(AuthContext);
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-    reset,
-  } = useForm();
-  // const [idError, setIdError] = useState([]);
-  // const [applierEmail, setApplierEmail] = useState([]);
-
-  const OverlayOne = () => (
-    <ModalOverlay
-      bg="blackAlpha.300"
-      // backdropFilter="blur(6px) hue-rotate(90deg)"
-    />
-  );
-
+  const OverlayOne = () => <ModalOverlay bg="blackAlpha.400" />;
+  const [idError, setIdError] = useState([]);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [overlay, setOverlay] = React.useState(<OverlayOne />);
-
-  // useEffect(() => {
-  //   fetch(`http://localhost:5000/users?email=${user?.email}`)
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setApplierEmail(data[0]);
-  //     });
-  // }, []);
-  // // if (loading) {
-  // //   return <Spinner />;
-  // // }
-
-  // // apply for credit card
-  // const handleApply = (data) => {
-  //   // event.preventDefault();
-  //   setIdError("");
-  //   const applierName = user?.displayName;
-  //   const accountId = data.accountId;
-  //   if (applierEmail?._id === accountId) {
-  //     const applierInfo = {
-  //       applierName,
-  //       accountId,
-  //     };
-  //     fetch(`http://localhost:5000/cardAppliers`, {
-  //       method: "POST",
-  //       headers: {
-  //         "content-type": "application/json",
-  //       },
-  //       body: JSON.stringify(applierInfo),
-  //     })
-  //       .then((res) => res.json())
-  //       .then((data) => {
-  //         if (data.acknowledged) {
-  //           console.log(data);
-  //           toast.success("Apply Success for card");
-  //           reset();
-  //         }
-  //       })
-  //       .then((error) => console.error(error));
-  //   } else {
-  //     setIdError("Account Id did't match");
-  //   }
-  // };
 
   return (
     <div className="credit-card-area py-16">
@@ -83,27 +23,41 @@ const ApplyCreditCard = () => {
               Apply for Credit Card
             </h2>
             <p className="py-3">
-              Porttitor ornare fermentum aliquam pharetra ut facilisis gravida
-              risus suscipit. dui feugiat fusce 
+              While you’ll always need some cash, using a credit card for your
+              spending needs can significantly simplify overseas travel.
             </p>
             <p className="flex font-semibold text-[#010C3A] mb-2 ">
               <span className="">
                 <AiOutlineCheckCircle className=" text-[#DF0303] font-bold text-[24px] p-[2px] rounded-md mr-1" />
               </span>
-              <span>The master-builder of great explorer</span>
+              <span>Avoid Dynamic Currency Conversion</span>
             </p>
             <p className="flex font-semibold text-[#010C3A] mb-2 ">
               <span className="">
                 <AiOutlineCheckCircle className=" text-[#DF0303] font-bold text-[24px] p-[2px] rounded-md mr-1" />
               </span>
-              <span>Velit esse cillum dolore eu fugiat nulla pariatur.</span>
+              <span>Call Your Credit Card Issuer Before Leaving</span>
             </p>
             <p className="flex font-semibold text-[#010C3A] mb-2 ">
               <span className="">
                 <AiOutlineCheckCircle className=" text-[#DF0303] font-bold text-[24px] p-[2px] rounded-md mr-1" />
               </span>
-              <span>Velit esse cillum dolore eu fugiat nulla pariatur.</span>
+              <span>Use a Credit Card with No Foreign Transaction Fee</span>
             </p>
+            <p className="flex font-semibold text-[#010C3A] mb-2 ">
+              <span className="">
+                <AiOutlineCheckCircle className=" text-[#DF0303] font-bold text-[24px] p-[2px] rounded-md mr-1" />
+              </span>
+              <span>
+                If a credit card is stolen, it can be suspended and replaced in
+                short order.
+              </span>
+            </p>
+            {idError && (
+              <p className="text-red-600 text-sm mb-0">
+                Account Id did't match Please Try Again.
+              </p>
+            )}
             <div>
               <button
                 className="primary-btn mt-5"
@@ -115,93 +69,19 @@ const ApplyCreditCard = () => {
                 Apply Now
               </button>
             </div>
-            <div className="my-2 mt-3">
-              {/* <form onSubmit={handleSubmit(handleApply)}>
-                <div>
-                  <div className="form-control ">
-                    <label className="text-base text-[#57647E]">
-                      Account Name
-                    </label>
-                    <input
-                      type="text"
-                      {...register("accountName", {
-                        required: "Account Name is required",
-                      })}
-                      className="border mb-2 mt-1 rounded w-full h-11 px-[10px]"
-                      placeholder={user?.displayName || "Please Sign In"}
-                      defaultValue={user?.displayName}
-                    ></input>
-                    {errors.accountName && (
-                      <p className="text-red-600 text-sm mb-0">
-                        {errors.accountName?.message}
-                      </p>
-                    )}
-                  </div>
-                  <div className="form-control  w-full">
-                    <label className="text-base text-[#57647E]">
-                      Phone Number
-                    </label>
-                    <input
-                      type="number"
-                      {...register("applierPhnNumber", {
-                        required: "Phone Number is required",
-                      })}
-                      className="border mb-2 mt-1 rounded w-full h-11 px-[10px]"
-                      placeholder="Phone Number"
-                    ></input>
-                    {errors.applierPhnNumber && (
-                      <p className="text-red-600 text-sm mb-0">
-                        {errors.applierPhnNumber?.message}
-                      </p>
-                    )}
-                  </div>
-                  <div className="form-control  w-full">
-                    <label className="text-base text-[#57647E]">
-                      Account Id
-                    </label>
-                    <input
-                      type="number"
-                      className="border mb-2 mt-1 rounded w-full h-11 px-[10px]"
-                      placeholder="Id Number"
-                      {...register("accountId", {
-                        required: "Account Id is Required",
-                        minLength: {
-                          value: 24,
-                          message: "Account Id Minimum 24 character",
-                        },
-                      })}
-                    ></input>
-                  </div>
-                  {errors.accountId && (
-                    <p className="text-red-600 text-sm mb-0">
-                      {errors.accountId?.message}
-                    </p>
-                  )}
-                  <div>
-                    <button
-                      className="primary-btn mt-5"
-                      onClick={() => {
-                        setOverlay(<OverlayOne />);
-                        onOpen();
-                      }}
-                    >
-                      Apply Now
-                    </button>
-                  </div>
-                </div>
-              </form> */}
-            </div>
           </div>
         </div>
       </div>
       {/* Modal */}
       <CreditCardModal
         onClose={onClose}
-        setOverlay={setOverlay}
+        // setOverlay={setOverlay}
         isOpen={isOpen}
         overlay={overlay}
-        onOpen={onOpen}
+        // onOpen={onOpen}
         OverlayOne={OverlayOne}
+        setIdError={setIdError}
+        idError={idError}
       />
     </div>
   );
--- src/component/CreditCardModal.js/CreditCardModal.js
+++ src/component/CreditCardModal.js/CreditCardModal.js
@@ -10,7 +10,15 @@ import { default as React, useContext, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 import { AuthContext } from "../../context/AuthProvider";
-const CreditCardModal = ({ onClose, onOpen, isOpen, overlay, setOverlay }) => {
+const CreditCardModal = ({
+  onClose,
+  onOpen,
+  isOpen,
+  overlay,
+  setOverlay,
+  setIdError,
+  idError,
+}) => {
   const { user, loading } = useContext(AuthContext);
   const {
     register,
@@ -18,15 +26,16 @@ const CreditCardModal = ({ onClose, onOpen, isOpen, overlay, setOverlay }) => {
     handleSubmit,
     reset,
   } = useForm();
-  const [idError, setIdError] = useState([]);
+  //   const [idError, setIdError] = useState([]);
   const [applierEmail, setApplierEmail] = useState([]);
   const [disable, setDisable] = useState(false);
   const OverlayOne = () => (
     <ModalOverlay
-      bg="blackAlpha.300"
+      bg="blackAlpha.700"
       //   backdropFilter="blur(10px) hue-rotate(90deg)"
     />
   );
+
 
   useEffect(() => {
     fetch(`http://localhost:5000/users?email=${user?.email}`)
@@ -42,16 +51,19 @@ const CreditCardModal = ({ onClose, onOpen, isOpen, overlay, setOverlay }) => {
   // apply for credit card
   const handleApply = (data) => {
     // event.preventDefault();
-    // setIdError("");
+    setIdError("");
     const applierName = user?.displayName;
     const applierPhnNumber = data.applierPhnNumber;
     const accountId = data.accountId;
+    const cardType = data.cardType;
     if (applierEmail?._id === accountId) {
       const applierInfo = {
         applierName,
         applierPhnNumber,
         accountId,
+        cardType,
       };
+      console.log(applierInfo, applierEmail?._id);
       fetch(`http://localhost:5000/cardAppliers`, {
         method: "POST",
         headers: {
@@ -64,12 +76,12 @@ const CreditCardModal = ({ onClose, onOpen, isOpen, overlay, setOverlay }) => {
           if (data.acknowledged) {
             console.log(data);
             toast.success("Apply Success for card");
-            // reset();
+            reset();
           }
         })
         .then((error) => console.error(error));
     } else {
-      setIdError("Account Id did't match");
+      toast.error("Account Id did't match");
     }
   };
 
@@ -79,120 +91,121 @@ const CreditCardModal = ({ onClose, onOpen, isOpen, overlay, setOverlay }) => {
         {overlay}
         <ModalContent>
           <ModalHeader>
-            {" "}
-            <h2 className="py-2 text-xl md:text-4xl font-semibold">
+            <h2 className="py-2 text-lg md:text-4xl font-semibold">
               Apply for Credit Card
             </h2>
           </ModalHeader>
           <ModalBody>
             <div className="mb-5">
               <form onSubmit={handleSubmit(handleApply)}>
-                <div>
-                  <div className="form-control ">
-                    <label className="text-base text-[#57647E]">
-                      Account Name
-                    </label>
-                    <input
-                      type="text"
-                      {...register("accountName", {
-                        required: "Account Name is required",
-                      })}
-                      className="border mb-2 mt-1 rounded w-full h-11 px-[10px]"
-                      placeholder={user?.displayName || "Please Sign In"}
-                      defaultValue={user?.displayName}
-                    ></input>
-                    {errors.accountName && (
-                      <p className="text-red-600 text-sm mb-0">
-                        {errors.accountName?.message}
-                      </p>
-                    )}
-                  </div>
-                  <div className="form-control  w-full">
-                    <label className="text-base text-[#57647E]">
-                      Phone Number
-                    </label>
-                    <input
-                      type="number"
-                      {...register("applierPhnNumber", {
-                        required: "Phone Number is required",
-                      })}
-                      className="border mb-2 mt-1 rounded w-full h-11 px-[10px]"
-                      placeholder="Phone Number"
-                    ></input>
-                    {errors.applierPhnNumber && (
-                      <p className="text-red-600 text-sm mb-0">
-                        {errors.applierPhnNumber?.message}
-                      </p>
-                    )}
-                  </div>
-                  <div className="form-control  w-full">
-                    <label className="text-base text-[#57647E]">
-                      Account Id
-                    </label>
-                    <input
-                      type="number"
-                      className="border mb-2 mt-1 rounded w-full h-11 px-[10px]"
-                      placeholder="Id Number"
-                      {...register("accountId", {
-                        required: "Account Id is Required",
-                        minLength: {
-                          value: 24,
-                          message: "Account Id Minimum 24 character",
-                        },
-                      })}
-                    ></input>
-                  </div>
-                  {errors.accountId && (
+                <div className="form-control ">
+                  <label className="text-base text-[#57647E]">
+                    Account Name
+                  </label>
+                  <input
+                    type="text"
+                    {...register("accountName", {
+                      required: "Account Name is required",
+                    })}
+                    className="border mb-2 mt-1 rounded w-full h-11 px-[10px]"
+                    placeholder={user?.displayName || "Please Sign In"}
+                    defaultValue={user?.displayName}
+                  ></input>
+                  {errors.accountName && (
                     <p className="text-red-600 text-sm mb-0">
-                      {errors.accountId?.message}
+                      {errors.accountName?.message}
                     </p>
                   )}
-                  <div className="form-control  w-full  md:mr-4">
-                    <label className="text-base text-[#57647E]">
-                      Account Type
-                    </label>
-                    <Select
-                      style={{
-                        margin: "5px 0 10px",
-                        width: "100%",
-                        height: "50px",
-                      }}
-                      placeholder="Account Type"
-                      className="from-select border  px-[10px] rounded "
-                      {...register("accountType", {
-                        required: "accountType is required",
-                      })}
-                    >
-                      <option value="Savings">Current</option>
-                      <option value="Savings">Savings</option>
-                      <option value="Others">Others</option>
-                    </Select>
-                  </div>
-                  <div className="mt-2">
+                </div>
+                <div className="form-control  w-full">
+                  <label className="text-base text-[#57647E]">
+                    Phone Number
+                  </label>
+                  <input
+                    type="number"
+                    {...register("applierPhnNumber", {
+                      required: "Phone Number is required",
+                    })}
+                    className="border mb-2 mt-1 rounded w-full h-11 px-[10px]"
+                    placeholder="Phone Number"
+                  ></input>
+                  {errors.applierPhnNumber && (
+                    <p className="text-red-600 text-sm mb-0">
+                      {errors.applierPhnNumber?.message}
+                    </p>
+                  )}
+                </div>
+                <div className="form-control  w-full">
+                  <label className="text-base text-[#57647E]">Account Id</label>
+                  <input
+                    type="text"
+                    className="border mb-2 mt-1 rounded w-full h-11 px-[10px]"
+                    placeholder="Id Number"
+                    {...register("accountId", {
+                      required: "Account Id is Required",
+                    })}
+                  ></input>
+                </div>
+                {errors.accountId && (
+                  <p className="text-red-600 text-sm mb-0">
+                    {errors.accountId?.message}
+                  </p>
+                )}
+                <div className="form-control  w-full  md:mr-4">
+                  <label className="text-base text-[#57647E]">Card Type</label>
+                  <Select
+                    style={{
+                      margin: "5px 0 10px",
+                      width: "100%",
+                      height: "50px",
+                    }}
+                    placeholder="Card Type"
+                    className="from-select border  px-[10px] rounded "
+                    {...register("cardType", {
+                      required: "Card Type is required",
+                    })}
+                  >
+                    <option value="CreditCard">Credit Card</option>
+                    <option value="DebitCard">Debit Card</option>
+                    <option value="MasterCard">Master Card</option>
+                    <option value="VISACard">VISA Card</option>
+                  </Select>
+                  {errors.cardType && (
+                    <p className="text-red-600 text-sm mb-0">
+                      {errors.cardType?.message}
+                    </p>
+                  )}
+                </div>
+                {/* {idError && (
+                    <p className="text-red-600 text-sm mb-0">
+                      Account Id did't match
+                    </p>
+                  )} */}
+                <div className="my-4">
+                  <button
+                    onClick={onClose}
+                    className="accent-btn sm-btn mt-5 mr-3"
+                  >
+                    Cancel
+                  </button>
+                  {disable ? (
                     <button
+                      className={`disable sm-btn mt-5 cursor-pointer`}
+                      type="submit"
                       onClick={onClose}
-                      className="accent-btn sm-btn mt-5 mr-3"
                     >
-                      Cancel
+                      Apply Now
                     </button>
-                    {disable ? (
-                      <button
-                        className={`disable sm-btn mt-5 cursor-pointer`}
-                        type="submit"
-                        onClick={onClose}
-                      >
-                        Apply Now
-                      </button>
-                    ) : (
-                      <button
-                        className="primary-btn sm-btn mt-5 cursor-pointer"
-                        type="submit"
-                        onClick={onClose}
-                      >
-                        Apply Now
-                      </button>
-                    )}
-                  </div>
+                  ) : (
+                    <button
+                      className="primary-btn sm-btn mt-5"
+                      value="Submit Form"
+                      type="submit"
+                      onClick={onClose}
+                    >
+                      Apply Now
+                    </button>
+                  )}
                 </div>
               </form>
             </div>
