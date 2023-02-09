53644e2b509772febce709d74136d6f18893e9d2
AkashChakrabortty <akashchakrabortty2000@gmail.com>
2023-02-09 16:55:48 +0600

    admin card

--- src/Dashboard/components/CustomerSupport/CustomerSupport.jsx
+++ src/Dashboard/components/CustomerSupport/CustomerSupport.jsx
@@ -1,21 +1,26 @@
 import { Avatar } from '@chakra-ui/react';
 import userEvent from '@testing-library/user-event';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../context/AuthProvider';
 
 const CustomerSupport = () => {
     const {user} = useContext(AuthContext);
+    const [adminInfo,setAdminInfo] = useState({})
+    useEffect(()=>{
+        fetch('http://localhost:5000/getAdminInfo')
+        .then(res=> res.json())
+        .then(data => setAdminInfo(data))
+    },[])
     return (
         <div>
             <Link to={`/dashboard/CustomerSupport/admin`}>
             <div className=" lg:w-1/2 mx-auto my-2 flex gap-2 h-fit border-2 p-2">
             <div className="photo">
-            <Avatar alt={user.displayName} src={user?.photoURL} />
+            <Avatar alt={adminInfo.name} src={adminInfo?.image} />
             </div>
             <div className="text">
-            <p>{user.displayName}</p>
-                <p>Who are you?</p>
+            <p>{adminInfo.name}</p>
             </div>
            </div>
             </Link>
