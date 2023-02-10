9fe4a44f931a27b2160a33653ebf1a7b6833cb3f
emon-webdev <emon.webdev@gmail.com>
2023-02-10 11:29:09 +0600

    donate modal add

--- src/Pages/FaqPage/FaqPage.js
+++ src/Pages/FaqPage/FaqPage.js
@@ -1,156 +1,208 @@
-import { Accordion, AccordionButton, AccordionItem,AccordionIcon,AccordionPanel, Box } from "@chakra-ui/react";
+import {
+  Accordion,
+  AccordionButton, AccordionIcon, AccordionItem, AccordionPanel,
+  Box
+} from "@chakra-ui/react";
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
-import logo from "../../assets/faq/faqImage.png"
+import logo from "../../assets/faq/faqImage.png";
 import DynamicBanner from "../Shared/DynamicBanner/DynamicBanner";
-import "./FaqPage.css"
+import "./FaqPage.css";
 
 const FaqPage = () => {
+
+  const { t } = useTranslation();
+
+
   return (
- <div>
-  <DynamicBanner name={"FAQ"}></DynamicBanner>
-   <div className="container my-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5 bg-faq p-5">
-    {/* faq image  */}
     <div>
-        <img src={logo} alt=""/>
+      <DynamicBanner name={"FAQ"}></DynamicBanner>
+      <div className="container my-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5 bg-faq p-5">
+        {/* faq image  */}
+        <div>
+          <img src={logo} alt="" />
+        </div>
+        {/* fag detail  */}
+        <div>
+          <h1 className="text-4xl font-semibold mb-3">
+            {t("Frequently_title")}
+          </h1>
+          <p>{t("freq_subtitle")}</p>
+          {/* Accordian  */}
+          <div className="my-5">
+            <Accordion defaultIndex={[0]} allowMultiple className="grid gap-5">
+              <AccordionItem>
+                <h2>
+                  <AccordionButton>
+                    <Box
+                      as="span"
+                      flex="1"
+                      textAlign="left"
+                      className="font-bold"
+                    >
+                      What is the minimum balance?
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                  Rationally encounter consequences that are extremely painful
+                  again there anyone who loves or pursues desire.
+                </AccordionPanel>
+              </AccordionItem>
+
+              <AccordionItem>
+                <h2>
+                  <AccordionButton>
+                    <Box
+                      as="span"
+                      flex="1"
+                      textAlign="left"
+                      className="font-bold"
+                    >
+                      What is the rate of interest?
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                  There anyone who loves or pursues desire rationally encounter
+                  consequences that are extremely painful again .
+                </AccordionPanel>
+              </AccordionItem>
+
+              <AccordionItem>
+                <h2>
+                  <AccordionButton>
+                    <Box
+                      as="span"
+                      flex="1"
+                      textAlign="left"
+                      className="font-bold"
+                    >
+                      When will I receive my account statement?
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                  Consequences that are extremely painful again there anyone
+                  rationally encounter who loves or pursues desire.
+                </AccordionPanel>
+              </AccordionItem>
+
+              <AccordionItem>
+                <h2>
+                  <AccordionButton>
+                    <Box
+                      as="span"
+                      flex="1"
+                      textAlign="left"
+                      className="font-bold"
+                    >
+                      Can I use any branch across India?
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                  Extremely painful again there anyone who rationally encounter
+                  consequences that are loves or pursues desire.
+                </AccordionPanel>
+              </AccordionItem>
+
+              <AccordionItem>
+                <h2>
+                  <AccordionButton>
+                    <Box
+                      as="span"
+                      flex="1"
+                      textAlign="left"
+                      className="font-bold"
+                    >
+                      How safe/secure is our net banking a/c?
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                  Anyone who loves or pursues desire rationally encounter
+                  consequences that are extremely painful again there .
+                </AccordionPanel>
+              </AccordionItem>
+
+              <AccordionItem>
+                <h2>
+                  <AccordionButton>
+                    <Box
+                      as="span"
+                      flex="1"
+                      textAlign="left"
+                      className="font-bold"
+                    >
+                      How you safe/secure is our net banking a/c?
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                  Pursues desire rationally encounter consequences that are
+                  anyone who loves or extremely painful again there .
+                </AccordionPanel>
+              </AccordionItem>
+
+              <AccordionItem>
+                <h2>
+                  <AccordionButton>
+                    <Box
+                      as="span"
+                      flex="1"
+                      textAlign="left"
+                      className="font-bold"
+                    >
+                      who loves or extremely painful again there?
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                  Anyone who loves or extremely painful again there pursues
+                  desire rationally encounter consequences that are.
+                </AccordionPanel>
+              </AccordionItem>
+
+              <AccordionItem>
+                <h2>
+                  <AccordionButton>
+                    <Box
+                      as="span"
+                      flex="1"
+                      textAlign="left"
+                      className="font-bold"
+                    >
+                      Encounter who loves or pursues desire
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                  That are extremely painful again there anyone rationally
+                  encounter who loves or pursues desire consequences .
+                </AccordionPanel>
+              </AccordionItem>
+            </Accordion>
+          </div>
+          {/* footer  */}
+          <div>
+            <span>
+              ✌️ By using question you to get{" "}
+              <Link className="text-blue-600">Any Information</Link>
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
-    {/* fag detail  */}
-    <div>
-        <h1 className="text-4xl font-semibold mb-3">Frequently Asked Questions
-what you need</h1>
-<p>Find answers to all your queries about our service.</p>
-{/* Accordian  */}
-<div className="my-5">
-<Accordion defaultIndex={[0]} allowMultiple className="grid gap-5">
-  <AccordionItem>
-    <h2>
-      <AccordionButton>
-        <Box as="span" flex='1' textAlign='left' className="font-bold">
-        What is the minimum balance?
-        </Box>
-        <AccordionIcon />
-      </AccordionButton>
-    </h2>
-    <AccordionPanel pb={4}>
-    Rationally encounter consequences that are extremely painful again there anyone who loves or pursues desire.
-    </AccordionPanel>
-  </AccordionItem>
-
-  <AccordionItem>
-    <h2>
-      <AccordionButton>
-        <Box as="span" flex='1' textAlign='left' className="font-bold">
-        What is the rate of interest?
-        </Box>
-        <AccordionIcon />
-      </AccordionButton>
-    </h2>
-    <AccordionPanel pb={4}>
-    There anyone who loves or pursues desire rationally encounter consequences that are extremely painful again .
-
-
-    </AccordionPanel>
-  </AccordionItem>
-
-  <AccordionItem>
-    <h2>
-      <AccordionButton>
-        <Box as="span" flex='1' textAlign='left' className="font-bold">
-When will I receive my account statement?
-        </Box>
-        <AccordionIcon />
-      </AccordionButton>
-    </h2>
-    <AccordionPanel pb={4}>
-    Consequences that are extremely painful again there anyone rationally encounter who loves or pursues desire.
-    </AccordionPanel>
-  </AccordionItem>
-
-
-  <AccordionItem>
-    <h2>
-      <AccordionButton>
-        <Box as="span" flex='1' textAlign='left' className="font-bold">
-Can I use any branch across India?
-        </Box>
-        <AccordionIcon />
-      </AccordionButton>
-    </h2>
-    <AccordionPanel pb={4}>
-    Extremely painful again there anyone who rationally encounter consequences that are loves or pursues desire.
-    </AccordionPanel>
-  </AccordionItem>
-
-
-  <AccordionItem>
-    <h2>
-      <AccordionButton>
-        <Box as="span" flex='1' textAlign='left' className="font-bold">
-How safe/secure is our net banking a/c?
-        </Box>
-        <AccordionIcon />
-      </AccordionButton>
-    </h2>
-    <AccordionPanel pb={4}>
-    Anyone who loves or pursues desire rationally encounter consequences that are extremely painful again there .
-    </AccordionPanel>
-  </AccordionItem>
-
-
-  <AccordionItem>
-    <h2>
-      <AccordionButton>
-        <Box as="span" flex='1' textAlign='left' className="font-bold">
-How you safe/secure is our net banking a/c?
-        </Box>
-        <AccordionIcon />
-      </AccordionButton>
-    </h2>
-    <AccordionPanel pb={4}>
-    Pursues desire rationally encounter consequences that are anyone who loves or extremely painful again there .
-    </AccordionPanel>
-  </AccordionItem>
-
-
-  <AccordionItem>
-    <h2>
-      <AccordionButton>
-        <Box as="span" flex='1' textAlign='left' className="font-bold">
-who loves or extremely painful again there?
-        </Box>
-        <AccordionIcon />
-      </AccordionButton>
-    </h2>
-    <AccordionPanel pb={4}>
-    Anyone who loves or extremely painful again there pursues desire rationally encounter consequences that are.
-    </AccordionPanel>
-  </AccordionItem>
-
-
-  <AccordionItem>
-    <h2>
-      <AccordionButton>
-        <Box as="span" flex='1' textAlign='left' className="font-bold">
-Encounter who loves or pursues desire
-        </Box>
-        <AccordionIcon />
-      </AccordionButton>
-    </h2>
-    <AccordionPanel pb={4}>
-    That are extremely painful again there anyone rationally encounter who loves or pursues desire consequences .
-    </AccordionPanel>
-  </AccordionItem>
-
-
-</Accordion>
-</div>
-{/* footer  */}
-<div>
-    <span>✌️ By using question you to get <Link className="text-blue-600">Any Information</Link></span>
-</div>
-    </div>
-  </div>
- </div>
   );
 };
 
--- src/component/Modal/DonateOptionModal.js
+++ src/component/Modal/DonateOptionModal.js
@@ -1,19 +1,151 @@
+import {
+    Modal,
+    ModalBody,
+    ModalContent,
+    ModalHeader,
+    Select
+} from "@chakra-ui/react";
 import React from "react";
+import { useForm } from "react-hook-form";
+const DonateOptionModal = ({ onClose, isOpen, onOpen }) => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm();
+  const [size, setSize] = React.useState("xl");
 
-const DonateOptionModal = () => {
+  // apply for credit card
+  const handleApply = (data) => {
+    // event.preventDefault();
+    const applierName = data.name;
+    const applierPhnNumber = data.applierPhnNumber;
+    const accountId = data.accountId;
+    const cardType = data.cardType;
+    const applierInfo = {
+      applierName,
+      applierPhnNumber,
+      accountId,
+      cardType,
+    };
+
+    console.log(applierInfo);
+  };
+
   return (
     <div>
-      <Modal onClose={onClose} size={size} isOpen={isOpen}>
-        <ModalOverlay />
+      <Modal
+        size={size}
+        isCentered
+        onClose={onClose}
+        isOpen={isOpen}
+        motionPreset="slideInBottom"
+      >
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
-          <ModalCloseButton />
+          <ModalHeader>
+            <h2 className="py-2 text-lg md:text-4xl font-semibold">
+              Apply for Credit Card
+            </h2>
+          </ModalHeader>
           <ModalBody>
-            <Lorem count={2} />
+            <div className="mb-5">
+              <div onSubmit={handleSubmit(handleApply)}>
+                <div className="form-control ">
+                  <label className="text-base text-[#57647E]">
+                    Account Name
+                  </label>
+                  <input
+                    type="text"
+                    {...register("accountName", {
+                      required: "Account Name is required",
+                    })}
+                    className="border mb-2 mt-1 rounded w-full h-11 px-[10px]"
+                    placeholder="Name"
+                  ></input>
+                  {errors.accountName && (
+                    <p className="text-red-600 text-sm mb-0">
+                      {errors.accountName?.message}
+                    </p>
+                  )}
+                </div>
+                <div className="form-control  w-full">
+                  <label className="text-base text-[#57647E]">
+                    Phone Number
+                  </label>
+                  <input
+                    type="number"
+                    {...register("applierPhnNumber", {
+                      required: "Phone Number is required",
+                    })}
+                    className="border mb-2 mt-1 rounded w-full h-11 px-[10px]"
+                    placeholder="Phone Number"
+                  ></input>
+                  {errors.applierPhnNumber && (
+                    <p className="text-red-600 text-sm mb-0">
+                      {errors.applierPhnNumber?.message}
+                    </p>
+                  )}
+                </div>
+                <div className="form-control  w-full">
+                  <label className="text-base text-[#57647E]">Account Id</label>
+                  <input
+                    type="text"
+                    className="border mb-2 mt-1 rounded w-full h-11 px-[10px]"
+                    placeholder="Id Number"
+                    {...register("accountId", {
+                      required: "Account Id is Required",
+                    })}
+                  ></input>
+                </div>
+                {errors.accountId && (
+                  <p className="text-red-600 text-sm mb-0">
+                    {errors.accountId?.message}
+                  </p>
+                )}
+                <div className="form-control  w-full  md:mr-4">
+                  <label className="text-base text-[#57647E]">Card Type</label>
+                  <Select
+                    style={{
+                      margin: "5px 0 10px",
+                      width: "100%",
+                      height: "50px",
+                    }}
+                    placeholder="Card Type"
+                    className="from-select border  px-[10px] rounded "
+                    {...register("cardType", {
+                      required: "Card Type is required",
+                    })}
+                  >
+                    <option value="CreditCard">Credit Card</option>
+                    <option value="DebitCard">Debit Card</option>
+                    <option value="MasterCard">Master Card</option>
+                    <option value="VISACard">VISA Card</option>
+                  </Select>
+                  {errors.cardType && (
+                    <p className="text-red-600 text-sm mb-0">
+                      {errors.cardType?.message}
+                    </p>
+                  )}
+                </div>
+                <div className="my-4">
+                  <button
+                    onClick={onClose}
+                    className="accent-btn sm-btn mt-5 mr-3"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    className={`disable sm-btn mt-5 cursor-pointer`}
+                    type="submit"
+                    onClick={onClose}
+                  >
+                    Apply Now
+                  </button>
+                </div>
+              </div>
+            </div>
           </ModalBody>
-          <ModalFooter>
-            <Button onClick={onClose}>Close</Button>
-          </ModalFooter>
         </ModalContent>
       </Modal>
     </div>
