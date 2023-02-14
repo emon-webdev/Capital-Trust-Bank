78c11347c434843acc4e8ee6afc07a1fcf1571c6
MD Rakib Khan <contact.antor.bd@gmail.com>
2023-02-14 14:38:55 +0600

    chat box design

--- src/Dashboard/components/CustomerSupport/CustomerSupport.jsx
+++ src/Dashboard/components/CustomerSupport/CustomerSupport.jsx
@@ -2,6 +2,8 @@ import { Avatar, WrapItem } from "@chakra-ui/react";
 import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../context/AuthProvider";
+import { FaLocationArrow } from 'react-icons/fa';
+import '../../../App.css'
 
 const CustomerSupport = () => {
   const { user, role } = useContext(AuthContext);
@@ -52,35 +54,35 @@ const CustomerSupport = () => {
     <>
       <div className="bg-white shadow-lg h-screen">
         <div className="p-3 border-b-2">
-          <WrapItem className="flex items-center">
+          <WrapItem className="flex items-center chat-avatar">
             <Avatar name={user?.name} src={user?.photoURL} />
             <h1 className="ml-3">{user?.displayName}</h1>
           </WrapItem>
         </div>
         {/* chat area */}
         {/* first message */}
-        <div className="flex flex-col space-y-4 p-3 overflow-y-auto scroll-w-2 scroll-touch">
+        <div className="flex flex-col space-y-4 p-3 overflow-y-auto scroll-w-2 scroll-touch h-[74vh]">
           <div className="chat-message">
-            <div className="flex items-end">
+            <div className="flex items-end py-3">
               <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
                 <div>
-                  <span className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-[#010c3a] text-white">lorem ipsum</span>
+                  <span className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-[#010c3a] text-white">hello akash vai. whats up?</span>
                 </div>
               </div>
               <img
-                className="w-6 h-6 rounded-full order-1"
+                className="w-9 h-9 rounded-full order-1"
                 src={user?.photoURL} alt={user?.displayName} />
             </div>
           </div>
         </div>
 
         {/* second message  */}
-        
-        <div className="flex flex-col space-y-4 p-3 overflow-y-auto scroll-w-2 scroll-touch">
+
+        {/* <div className="flex flex-col space-y-4 p-3 overflow-y-auto scroll-w-2 scroll-touch">
           <div className="chat-message">
             <div className="flex items-end justify-end">
               <div>
-                
+
                 <img
                   className="w-6 h-6 rounded-full order-1"
                   src={user?.photoURL} alt={user?.displayName} />
@@ -92,6 +94,17 @@ const CustomerSupport = () => {
                 </div>
               </div>
             </div>
+          </div>
+        </div> */}
+
+        {/* button  */}
+        <div className="p-3">
+          <hr />
+          <div className="relative">
+            <input
+              placeholder="Write Something...."
+              className=" w-full mt-3 active:outline-none focus:outline-none focus:placeholder-gray-400 placeholder-gray-300  bg-gray-100 px-4 rounded-full py-3 border-gray-200" />            
+              <FaLocationArrow className='dd absolute right-[3%] top-[43%] text-[#010c3a] text-[22px] cursor-pointer' />           
           </div>
         </div>
       </div>
