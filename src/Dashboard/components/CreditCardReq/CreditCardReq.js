06ae736bcd50c275225e808b18b4d034e79650f1
AkashChakrabortty <akashchakrabortty2000@gmail.com>
2023-02-15 18:13:13 +0600

    card req show

--- src/Dashboard/components/CreditCardReq/CreditCardReq.js
+++ src/Dashboard/components/CreditCardReq/CreditCardReq.js
@@ -1,4 +1,6 @@
+import { Table, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import '../../../App.css';
 
 const CreditCardReq = () => {
@@ -16,28 +18,58 @@ const CreditCardReq = () => {
         <h2 className="text-center heading">
           Total Card Request:{customers.length}
         </h2>
-      <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-3 sm:grid-cols-1'>
-      {customers.map((customer) => {
-          return (
-            <div key={customer._id}>
-              <div className=" mx-auto my-2 gap-4 py-5 px-5 rounded-xl h-fit bg-white">
-                <div className="info flex flex-col overflow-auto">
-                  <span className='py-2 text-xl'>Name: {customer.applierName}</span>
-                  <span className='text-lg'>id:  {customer.accountId}</span>
-                  <div className="flex gap-3 mt-3 p-3">
-                    <button className="text-lg fw-bold rounded sm-btn primary-btn exchange-btn accept bg-[#010c3a]">
-                      Accept
-                    </button>
-                    <button className="text-md sm-btn primary-btn exchange-btn bg-[#df0303]">
-                      Cancel
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+        <div className="w-full">
+          <TableContainer
+            borderRadius={6}
+            style={{ boxShadow: "0 4px 4px rgb(87 100 126 / 21%" }}
+            backgroundColor="white"
+            marginY={10}
+            // marginLeft={20}
+            height={500}
+            overflowY="scroll"
+            overflowX="scroll"
+          >
+            <Table variant="simple">
+              <Thead>
+                <Tr>
+                <Th color="#041C51" fontSize={16}>
+                    List
+                  </Th>
+                  <Th color="#041C51" fontSize={16}>
+                    Name
+                  </Th>
+                  <Th color="#041C51" fontSize={16}>
+                    Phone
+                  </Th>
+                  <Th color="#041C51" fontSize={16}>
+                  Id
+                  </Th>
+                  <Th color="#041C51" fontSize={16}>
+                  Card Type
+                  </Th>
+                  <Th color="#041C51" fontSize={16}>
+                  Action
+                  </Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {customers?.map((customer, index) => (
+                  <Tr key={customer?._id}>
+                    <Td>{index + 1}</Td>
+                    <Td>{customer.applierName}</Td>
+                    <Td>{customer.applierPhnNumber}</Td>
+                    <Td>{customer?.accountId}</Td>
+                    <Td>{customer?.cardType}</Td>
+                    <Td>
+                    <button class="text-lg fw-bold rounded sm-btn primary-btn exchange-btn accept bg-[#010c3a] m-1">Accept</button>
+                    <button class="text-md sm-btn primary-btn exchange-btn bg-[#df0303]">Cancel</button>
+                    </Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </div>
       </div>
     );
 };
