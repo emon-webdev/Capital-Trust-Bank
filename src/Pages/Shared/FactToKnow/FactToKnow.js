06cc0228a211cd0c3752a1a01981bee816cd169b
wahid30 <md.wahidulalamjp2@gmail.com>
2023-02-16 15:31:42 +0600

    facttoknow done

--- src/Pages/Shared/FactToKnow/FactToKnow.js
+++ src/Pages/Shared/FactToKnow/FactToKnow.js
@@ -8,9 +8,9 @@ function FactToKnow() {
       <div className="container py-5 grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 gap-5">
         <div>
         <div className="flex gap-2 mt-5">
-            <span className="custom-hypen">-</span>
+            <span className="custom-hypen">--</span>
             <h3 className="small-title">Fact to know</h3>
-            <span className="custom-hypen">-</span>
+            <span className="custom-hypen">--</span>
         </div>
         <h1 className="big-title mt-5">Apply for an account in minutes</h1>
         <p className="fact-paragraph mt-5">Business is the activity of making one's living or making money by producing or buying and selling products</p>
