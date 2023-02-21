8db102f93429872ff8134bfd572d2b800cbf703d
Salma Tabassum Mouri <mouriclub@gmail.com>
2023-02-22 00:53:11 +0600

    ad

--- src/Pages/Home/Services.jsx
+++ src/Pages/Home/Services.jsx
@@ -10,13 +10,13 @@ import {  useGetLoansQuery } from '../../features/loansSlice';
 const Services = () => {
 
   const [loans, setLoans] = useState([]);
-  // useEffect(() => {
-  //   fetch('http://localhost:5000/loans')
-  //     .then(res => res.json())
-  //     .then(data => setLoans(data))
-  // }, [])
+  useEffect(() => {
+    fetch('http://localhost:5000/loans')
+      .then(res => res.json())
+      .then(data => setLoans(data))
+  }, [])
 
-  const {isLoading}= useGetLoansQuery();
+ 
   
 
   return (
