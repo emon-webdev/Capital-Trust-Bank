601330a861d02f18642c313ef755517df4e20781
emon-webdev <emon.webdev@gmail.com>
2023-02-22 16:58:27 +0600

    merge Mouri

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
 export const loanApi = createApi({
   reducerPath: "loanApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000",
+    baseUrl: `http://localhost:5000`,
   }),
   endpoints: (builder) => ({
     getLoans: builder.query({
